93b50ffe7669db63ec0041bb90b14f99b23d9069
Yalz <Yalz@users.noreply.github.com>
2025-01-27 12:06:57 +0100

    feat: aoc 2024: day 8

--- 2024-py/day_8/antenna/antenna.py
+++ 2024-py/day_8/antenna/antenna.py
@@ -1,4 +1,5 @@
 import copy
+import math
 
 import numpy as np
 
@@ -7,23 +8,33 @@ class Antenna:
 	anti_node_map = []
 	anti_node_count = 0
 
-	def __init__(self, freq_map):
+
+	def __init__(self, freq_map, ext=False):
 		self.freq_map = np.array(freq_map)
 		self.anti_node_map = copy.copy(self.freq_map)
 		print()
 		print("Antenna initialized")
 		self.frequencies = np.unique(self.freq_map)
 		self.frequencies = np.delete(self.frequencies, np.where(self.frequencies == '.'))
-		self.generate_anti_node_amp()
+		self.frequencies = np.delete(self.frequencies, np.where(self.frequencies == '#'))
+		self.rows, self.cols = np.shape(self.freq_map)
+		self.generate_anti_node_amp(ext)
 		pass
 
 	def assign_anti_node(self, an_spot):
-		rows, cols = np.shape(self.freq_map)
-
-		if 0 <= an_spot[0] < rows and 0 <= an_spot[1] < cols:
+		if 0 <= an_spot[0] < self.rows and 0 <= an_spot[1] < self.cols:
 			self.anti_node_map[an_spot[0]][an_spot[1]] = '#'
 
-	def generate_anti_node_amp(self):
+	def assign_anti_node_arr(self, start, direction):
+		gcd_value = math.gcd(direction[0], direction[1])
+		direction = direction // gcd_value
+		while True:
+			if 0 <= start[0] < self.rows and 0 <= start[1] < self.cols:
+				self.anti_node_map[start[0]][start[1]] = '#'
+				start = start + direction
+			else:
+				break
+	def generate_anti_node_amp(self, ext=False):
 		for frequency in self.frequencies:
 			freq_positions = np.argwhere(self.freq_map == frequency)
 			for freq_pos in freq_positions:
@@ -35,12 +46,17 @@ class Antenna:
 					from_v = other_freq_pos - freq_pos
 					an_spot = other_freq_pos + from_v
 					self.assign_anti_node(an_spot)
+					if ext:
+						self.assign_anti_node_arr(other_freq_pos, from_v)
 
 					to_v = freq_pos - other_freq_pos
 					an_spot = freq_pos + to_v
 
 					self.assign_anti_node(an_spot)
+					if ext:
+						self.assign_anti_node_arr(freq_pos, to_v)
 
 		frequency, counts = np.unique(self.anti_node_map, return_counts=True)
 		unique_counts_dict = dict(zip(frequency, counts))
 		self.anti_node_count = unique_counts_dict['#']
+
--- 2024-py/day_8/antenna/antenna_test.py
+++ 2024-py/day_8/antenna/antenna_test.py
@@ -9,8 +9,18 @@ def test_generate_freq_map():
 	freq_grid = file_utils.file_to_line_arr(file_path, True)
 
 	antenna = Antenna(freq_grid)
+	antenna_ext = Antenna(freq_grid, True)
 
 	assert antenna.anti_node_count == 14
+	assert antenna_ext.anti_node_count == 34
+
+def test_generate_freq_map_ext():
+	file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../data/example-pt2.txt'))
+	freq_grid = file_utils.file_to_line_arr(file_path, True)
+
+	antenna_ext = Antenna(freq_grid, True)
+
+	assert antenna_ext.anti_node_count == 9
 
 def test_input():
 	file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../data/input.txt'))
@@ -19,4 +29,9 @@ def test_input():
 	antenna = Antenna(freq_grid)
 
 	print("Part 1", antenna.anti_node_count)
+	assert antenna.anti_node_count == 303
+
+	antenna = Antenna(freq_grid, True)
+
+	print("Part 2", antenna.anti_node_count)
 	assert antenna.anti_node_count == 303
